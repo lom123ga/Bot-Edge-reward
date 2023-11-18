44545ce7ef46a70844d179a6f944e4a0c159ea05
Triệu Ngọc Tâm <89369266+lom123ga@users.noreply.github.com>
2023-11-19 01:52:09 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,60 @@
+[![CodeFactor](https://www.codefactor.io/repository/github/tmxkn1/microsoft-reward-chrome-ext/badge)](https://www.codefactor.io/repository/github/tmxkn1/microsoft-reward-chrome-ext)
+
+# Microsoft Rewards Bot
+
+A Chrome extension for Microsoft Rewards, which automatically clears PC, mobile and Edge search quests and displays daily point countdown.
+
+This extension will not clear any promotional link or quiz quests and this feature will not be implemented.
+
+**This is a bot!**
+
+Use at your own risk. I do not take responsibility for any consequence caused by this extension.
+
+# Install from GitHub
+
+1. Head to [Release](https://github.com/tmxkn1/Microsoft-Reward-Chrome-Ext/releases) and download the latest Source code (zip).
+2. Unzip the downloaded file to somewhere you want to keep it.
+3. Open Chrome.
+4. Go to chrome://extensions/ and enable `Developer mode`.
+5. Select `Load unpacked` and select the `src` folder.
+6. Enable the extension.
+
+
+# Install from Chrome web store
+
+Unfortunately, the extension could not make its way to become visible on web store due to ... well ... obvious reasons.
+
+# Use
+
+1. Log into [Microsoft Rewards](https://account.microsoft.com/rewards) and [Bing](https://www.bing.com/).
+2. Enjoy.
+
+*Note: You may need to repeat step 1 every time you restart Chrome.*
+
+# Functionality
+
+### Background Work
+
+The extension checks your daily Microsoft Rewards progress every two hours and clears any incomplete search quests. When you have any available quiz/link quests yet to complete, a yellow badge will show with a number indicating the amount of points they worth.
+
+### Action Button
+
+1. A button to force check reward progress.
+2. A link to Microsoft Rewards.
+3. Options:
+   1. Compatibility Mode. Toggle to enable/disable a different way of calculating point countdown. Try this if you have any issues with the current point countdown method.
+   2. Copy Debug Info. This will place a bunch of information in your clipboard. The information is not useful for end users.
+
+### Action Button Badge
+
+The colour of the badge changes depending on your daily reward progress or the status of the extension:
+
+![greyLogo](https://github.com/tmxkn1/Microsoft-Reward-Chrome-Ext/blob/master/src/img/grey@1x.png?raw=true) - Just hang on a second. The extension will start to work soon.
+
+![blueLogo](https://github.com/tmxkn1/Microsoft-Reward-Chrome-Ext/blob/master/src/img/busy@1x.png?raw=true) - Working hard on doing those search quests for ya!
+
+![greenLogo](https://github.com/tmxkn1/Microsoft-Reward-Chrome-Ext/blob/master/src/img/done@1x.png?raw=true) - Congratulations! You have completed all quests for today.
+
+![yellowLogo](https://github.com/tmxkn1/Microsoft-Reward-Chrome-Ext/blob/master/src/img/warn@1x.png?raw=true) - Attention! You still have some points to earn through quests.
+
+![redLogo](https://github.com/tmxkn1/Microsoft-Reward-Chrome-Ext/blob/master/src/img/err@1x.png?raw=true) - Opps... an error occurred - Have you logged into your Microsoft account? Try log into [Microsoft Rewards](https://account.microsoft.com/rewards) and [Bing](www.bing.com) and use the Check Now button to force it do another round of work. If the error persists, please report it to me. Thanks!
---
+++ src/quest/searchQuest.js
@@ -0,0 +1,258 @@
+class SearchQuest {
+    constructor(googleTrend) {
+        this._googleTrend_ = googleTrend;
+        this._searchIntervalMS = 2000;
+        this.reset();
+    }
+
+    reset() {
+        this._status_ = null;
+        this._pcSearchWordIdx_ = 0;
+        this._mbSearchWordIdx_ = 0;
+        this._currentSearchCount_ = 0;
+        this._currentSearchType_ = null;
+        this._jobStatus_ = STATUS_NONE;
+    }
+
+    get jobStatus() {
+        return this._jobStatus_;
+    }
+
+    async doWork(status) {
+        console.assert(status != null);
+
+        this._status_ = status;
+        this._jobStatus_ = STATUS_BUSY;
+        try {
+            await getUA();
+            await this._googleTrend_.getGoogleTrendWords();
+            await this._doWorkLoop();
+        } catch (ex) {
+            this._jobStatus_ = STATUS_ERROR;
+            if (ex instanceof UserAgentInvalidException) {
+                notifyUpdatedUAOutdated();
+            }
+            throw ex;
+        }
+    }
+
+    async _doWorkLoop() {
+        while (true) {
+            if (this._status_.isSearchCompleted) {
+                return;
+            }
+
+            if (this._status_.jobStatus == STATUS_ERROR || !this._status_.summary.isValid) {
+                this._jobStatus_ = STATUS_ERROR;
+                return;
+            }
+
+            await this._startSearchQuests();
+
+            const flag = await this.isSearchSuccessful();
+            if (flag > 0) {
+                await this._getAlternativeUA(flag);
+            }
+        }
+    }
+
+    async _startSearchQuests() {
+        await this._doPcSearch();
+        await this._doMbSearch();
+        this._quitSearchCleanUp();
+    }
+
+    async isSearchSuccessful() {
+        // Return:
+        // 0 - successful; 1 - pc search failed; 2 - mb search failed; 3 - both failed
+        const pcSearchProgBefore = this._status_.pcSearchStatus.progress;
+        const mbSearchProgBefore = this._status_.mbSearchStatus.progress;
+        await this._status_.update();
+        const flag = (!this._status_.pcSearchStatus.isValidAndCompleted && (pcSearchProgBefore == this._status_.pcSearchStatus.progress));
+        return flag + 2 * (!this._status_.mbSearchStatus.isValidAndCompleted && (mbSearchProgBefore == this._status_.mbSearchStatus.progress));
+    }
+
+    async _getAlternativeUA(flag) {
+        if (flag == 3) {
+            if (userAgents.pcSource == 'updated' && userAgents.mbSource == 'updated') {
+                throw new UserAgentInvalidException('Cannot find working UAs for pc and mobile.');
+            }
+            await getUpdatedUA('both');
+        } else if (flag == 1) {
+            if (userAgents.pcSource == 'updated') {
+                throw new UserAgentInvalidException('Cannot find a working UA for pc.');
+            }
+            await getUpdatedUA('pc');
+        } else if (flag == 2) {
+            if (userAgents.mbSource == 'updated') {
+                throw new UserAgentInvalidException('Cannot find a working UA for mobile.');
+            }
+            await getUpdatedUA('mb');
+        }
+        notifyStableUAOutdated(flag);
+    }
+
+    async _doPcSearch() {
+        this._initiateSearch();
+        if (this._currentSearchType_ != SEARCH_TYPE_PC_SEARCH) {
+            this._preparePCSearch();
+        }
+
+        await this._requestBingSearch();
+    }
+
+    async _doMbSearch() {
+        this._initiateSearch();
+        if (this._currentSearchType_ != SEARCH_TYPE_MB_SEARCH) {
+            this._prepareMbSearch();
+        }
+
+        await this._requestBingSearch();
+    }
+
+    _initiateSearch() {
+        this._currentSearchCount_ = 0;
+    }
+
+    _preparePCSearch() {
+        this._currentSearchType_ = SEARCH_TYPE_PC_SEARCH;
+        removeUA();
+        setMsEdgeUA();
+    }
+
+    _prepareMbSearch() {
+        this._currentSearchType_ = SEARCH_TYPE_MB_SEARCH;
+        removeUA();
+        setMobileUA();
+    }
+
+    _quitSearchCleanUp() {
+        if (this._jobStatus_ == STATUS_BUSY) {
+            this._jobStatus_ = STATUS_DONE;
+        }
+        this._currentSearchType_ = null;
+        removeUA();
+    }
+
+    async _requestBingSearch() {
+        if (this._isCurrentSearchCompleted()) {
+            return;
+        }
+        let response;
+        try {
+            response = await fetch(this._getBingSearchUrl());
+        } catch (ex) {
+            throw new FetchFailedException('Search', ex);
+        }
+
+        if (response.status != 200) {
+            throw new FetchResponseAnomalyException('Search');
+        }
+
+        this._currentSearchCount_++;
+        await sleep(this._searchIntervalMS);
+
+        await this._requestBingSearch();
+    }
+
+    _getBingSearchUrl() {
+        const word = this._currentSearchType_ == SEARCH_TYPE_PC_SEARCH ?
+            this._googleTrend_.nextPCWord :
+            this._googleTrend_.nextMBWord;
+
+        return `https://www.bing.com/search?q=${word}&form=QBRE`;
+    }
+
+    _isCurrentSearchCompleted() {
+        return this._currentSearchType_ == SEARCH_TYPE_PC_SEARCH ?
+            this._currentSearchCount_ >= this._status_.pcSearchStatus.searchNeededCount :
+            this._currentSearchCount_ >= this._status_.mbSearchStatus.searchNeededCount;
+    }
+}
+
+function removeUA() {
+    try {
+        chrome.webRequest.onBeforeSendHeaders.removeListener(toMobileUA);
+    } catch (ex) { }
+    try {
+        chrome.webRequest.onBeforeSendHeaders.removeListener(toMsEdgeUA);
+    } catch (ex) { }
+}
+
+function setMsEdgeUA() {
+    chrome.webRequest.onBeforeSendHeaders.addListener(toMsEdgeUA, {
+        urls: ['https://www.bing.com/search?q=*'],
+    }, ['blocking', 'requestHeaders']);
+}
+
+function toMsEdgeUA(details) {
+    for (const i in details.requestHeaders) {
+        if (details.requestHeaders[i].name === 'User-Agent') {
+            details.requestHeaders[i].value = userAgents.pc;
+            break;
+        }
+    }
+    return {
+        requestHeaders: details.requestHeaders,
+    };
+}
+
+function setMobileUA() {
+    chrome.webRequest.onBeforeSendHeaders.addListener(toMobileUA, {
+        urls: ['https://www.bing.com/search?q=*'],
+    }, ['blocking', 'requestHeaders']);
+}
+
+function toMobileUA(details) {
+    for (const i in details.requestHeaders) {
+        if (details.requestHeaders[i].name === 'User-Agent') {
+            details.requestHeaders[i].value = userAgents.mb;
+            break;
+        }
+    }
+    return {
+        requestHeaders: details.requestHeaders,
+    };
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function notifyStableUAOutdated(flag) {
+    if (developer && developer.notification_ua_stable_outdated) {
+        const message = 'Stable UA is outdated! Flag: ' + (flag == 3 ? 'pc and mobile' : flag == 1 ? 'pc' : 'mobile');
+        console.log(message);
+        chrome.notifications.clear('stable_ua_outdated');
+        chrome.notifications.create('stable_ua_outdated', {
+            type: 'basic',
+            iconUrl: 'img/warn@8x.png',
+            title: 'Developer notification',
+            message: message,
+            priority: 2,
+        });
+    }
+}
+
+function notifyUpdatedUAOutdated() {
+    if (developer && developer.notification_ua_updated_outdated) {
+        const message = 'Critical!! Updated UA is outdated!';
+        console.log(message);
+        chrome.notifications.clear('updated_ua_outdated');
+        chrome.notifications.create('updated_ua_outdated', {
+            type: 'basic',
+            iconUrl: 'img/err@8x.png',
+            title: 'Developer notification',
+            message: message,
+            priority: 2,
+        });
+    }
+}
+
+const SEARCH_TYPE_PC_SEARCH = 0;
+const SEARCH_TYPE_MB_SEARCH = 1;
+const STATUS_NONE = 0;
+const STATUS_BUSY = 1;
+const STATUS_DONE = 20;
+const STATUS_WARNING = 30;
+const STATUS_ERROR = 3;
